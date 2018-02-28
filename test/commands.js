7147d612cadf90929e29d30f839304df1fc536bb
Javon Davis <javonldavis14@gmail.com>
2018-02-28 15:44:45 -0500

    fixed expected object in test that caused AssertionError when running npm test

--- test/commands.js
+++ test/commands.js
@@ -5,18 +5,26 @@ import should from 'should';
 var index = rewire('../src/index.js');
 var parseCommands = index.__get__('_parseCommandArgs');
 
-describe("Testing parsing of command line args", function () {
+describe('Testing parsing of command line args', function () {
 
-    it("should return default configurations when there are no arguments.", function () {
-        var _process = {};
-        _process.argv = [];
+  it('should return default configurations when there are no arguments.', function () {
+    var _process = {};
+    _process.argv = [];
 
-        parseCommands(_process).should.eql({
-            isHelp: false,
-            fileData: {},
-            verbose: false,
-            report: false,
-            url: []
-        });
-    })
-})
+    parseCommands(_process).should.eql({
+      isHelp: false,
+      fileData: {
+        cookies: null,
+        output_to_screen: true,
+        save_to_file: null,
+        screenshot: false,
+        timeout: 15,
+        urls: [],
+        whitleist: null
+      },
+      verbose: false,
+      report: false,
+      url: []
+    });
+  })
+});
